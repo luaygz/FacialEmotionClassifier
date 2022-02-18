71914a7ff65ee221b655a1e157778400fe550f5d
Luay Gharzeddine <lmg11@mail.aub.edu>
2022-02-18 08:54:22 +0200

    Removed ununsed method

--- callbacks.py
+++ callbacks.py
@@ -19,26 +19,3 @@ class TelegramNotifier(Callback):
             telegram_send.send(messages=[message])
         except:
             print("Could not send telegram message.")
-
-class F1History(Callback):
-    def __init__(self, train, validation=None):
-        super(F1History, self).__init__()
-        self.validation = validation
-        self.train = train
-
-    def on_epoch_end(self, epoch, logs={}):
-
-        logs['F1_score_train'] = float('-inf')
-        X_train, y_train = self.train[0], self.train[1]
-        y_pred = (self.model.predict(X_train).ravel()>0.5)+0
-        score = f1_score(y_train, y_pred)       
-
-        if (self.validation):
-            logs['F1_score_val'] = float('-inf')
-            X_valid, y_valid = self.validation[0], self.validation[1]
-            y_val_pred = (self.model.predict(X_valid).ravel()>0.5)+0
-            val_score = f1_score(y_valid, y_val_pred)
-            logs['F1_score_train'] = np.round(score, 5)
-            logs['F1_score_val'] = np.round(val_score, 5)
-        else:
-            logs['F1_score_train'] = np.round(score, 5)
